435de96df9a225e87e1956cf5f2423310ace3734
SuperCoolMan <101731621+iSuperCoolMan@users.noreply.github.com>
2023-05-13 17:48:25 +0400

    Initial commit

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _spawnObject;
+    [SerializeField] private float _spawnDelayInSeconds;
+
+    private List<Transform> _spawnPoints;
+
+    private void Start()
+    {
+        _spawnPoints = GetComponentsInChildren<Transform>().ToList();
+        _spawnPoints.Remove(_spawnPoints[0]);
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        var waitingTime = new WaitForSeconds(_spawnDelayInSeconds);
+
+        while (true)
+        {
+            foreach(Transform spawnPoint in _spawnPoints)
+            {
+                Instantiate(_spawnObject, spawnPoint.position, spawnPoint.rotation);
+                yield return waitingTime;
+            }
+        }
+    }
+}
